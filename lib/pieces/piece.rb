1195c62c777d5b7e68a1c1cc0daf9ad33b4d3911
Colin Ayres <88121502+chayre@users.noreply.github.com>
2022-05-05 21:49:32 -0400

    added more comments

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -3,6 +3,7 @@
 # Chess piece class from which all other pieces will inherit
 class ChessPiece
   attr_accessor :color, :moveset, :x_position, :y_position, :possible_moves, :icon
+  
   def initialize(color, position)
     # Color is false for black (absence of color) and true for white
     @color = color ? 'white' : 'black'
@@ -15,7 +16,7 @@ class ChessPiece
   # Based on the moveset, calculate the possible moves and add it to this piece's possible_moves array. There are three conditions:
   # 1. Cannot move off the board
   # 2. Cannot move onto another piece of the same color
-  # 3. Cannot put its own King into check -- but we'll check this in another method. This method covers conditions 1 and 2.
+  # 3. Cannot put its own King into check -- but we'll check this in another method, in chess.rb. This method covers conditions 1 and 2.
   def find_possible_moves(positions)
     @possible_moves = []
     # For each move in the defined movesets (will be defined individually for each unique piece)
@@ -27,8 +28,10 @@ class ChessPiece
         break unless (0..7).cover?(x) && (0..7).cover?(y)
         # This line adds the move to the possible_moves array if the desired position is empty or is occupied by a piece of the opposite color
         @possible_moves << [x, y] if positions[x][y].nil? || positions[x][y].color != @color
-        # The line below specifies this move is invalid if the desired 
+        # For pieces that can move in a straight line across the board, like the rook, we need to check if its possible move will hit a piece
+        # Otherwise, it might be able to move through a line of pawns into the back line of enemy pieces, which is an invalid move
         break if !positions[x][y].nil?
+        # As long as we haven't hit any pieces, we'll keep adding moves in this straight line
         x += move[0]
         y += move[1]
       end
