4b889261640b669ba7a0400d226b6de3ebb3231c
seairs <sea.airs77@gmail.com>
2022-05-07 18:35:29 -0400

    adding board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -26,9 +26,18 @@ RSpec.describe Board do
       expect(board.positions[7].all? {|piece| piece.color == 'white'}).to be true
     end
 
+    it 'has a second to top row of pawns' do
+        expect(board.positions[1].all? { |piece| piece.instance_of?(Pawn) }).to be true
+      end
+
+    it 'has a second to bottom row of pawns' do
+      expect(board.positions[6].all? { |piece| piece.instance_of?(Pawn) }).to be true
+    end
+
+    it 'has a has rooks in the correct position' do
+        expect(board.positions[0][0].instance_of?(Rook) && board.positions[0][7].instance_of?(Rook) && board.positions[7][0].instance_of?(Rook) && board.positions[7][7].instance_of?(Rook)).to be true
+      end
 
 
-
-    
   end
 end
