93d737c6e347d1f29ea42aa92dba60e406724516
seairs <sea.airs77@gmail.com>
2022-05-31 01:24:58 -0400

    update queen test spec to remove, fix bug with pawn positions for black queen tests

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -45,52 +45,36 @@ end
 
 # Create a black Queen to check proper movement
 describe Queen do
-  let(:board) { Board.new(true) }
+  let(:nboard) { Board.new(true) }
   let(:bqueen) { Queen.new(false, [4, 4]) }
   before do
-    board.positions[4][4] = bqueen
+    nboard.positions[4][4] = bqueen
   end
 
   context 'black queen movement calculations' do
-    # Check that the Queen moves as expected when no pieces are in the way
-    it 'should move diagonally/laterally from its starting coordinate' do
-      bqueen.find_possible_moves(board.positions)
-      expect(bqueen.possible_moves.sort).to eq([[0, 0], [0, 4], [1, 1], [1, 4],
-                                                [1, 7], [2, 2], [2, 4], [2, 6],
-                                                [3, 3], [3, 4], [3, 5], [4, 0],
-                                                [4, 3], [4, 5], [4, 6], [4, 7],
-                                                [5, 3], [5, 4], [5, 5], [6, 2],
-                                                [6, 4], [6, 6], [7, 1], [7, 4],
-                                                [7, 7]])
-    end
-
     # Check that the Queen cannot move through or onto pieces of the same color
     it 'should not move through or onto pieces of the same color' do
-      board.positions[5][2] = Pawn.new(false, [2, 4])
-      board.positions[4][1] = Pawn.new(false, [4, 3])
-      bqueen.find_possible_moves(board.positions)
-      expect(bqueen.possible_moves.sort).to eq([[0, 0], [0, 4], [1, 1], [1, 4],
-                                                [1, 7], [2, 2], [2, 4], [2, 6],
-                                                [3, 3], [3, 4], [3, 5], [4, 2],
-                                                [4, 3], [4, 5], [4, 6], [4, 7],
-                                                [5, 3], [5, 4], [5, 5], [6, 2],
-                                                [6, 4], [6, 6], [7, 1], [7, 4],
-                                                [7, 7]])
+      nboard.positions[2][4] = Pawn.new(false, [2, 4])
+      nboard.positions[4][3] = Pawn.new(false, [4, 3])
+      bqueen.find_possible_moves(nboard.positions)
+      expect(bqueen.possible_moves.sort).to eq([[0, 0], [1, 1], [1, 7], [2, 2],
+                                                [2, 6], [3, 3], [3, 4], [3, 5],
+                                                [4, 5], [4, 6], [4, 7], [5, 3],
+                                                [5, 4], [5, 5], [6, 2], [6, 4],
+                                                [6, 6], [7, 1], [7, 4], [7, 7]])
     end
 
-    # Check that the queen cannot move through pieces of the opposing color but can capture them
+    # Check that the Queen cannot move through pieces of the opposing color but can capture them
     it 'should not move through pieces of the opposing color, but can move onto them' do
-      board.positions[5][2] = Pawn.new(true, [2, 4])
-      board.positions[4][1] = Pawn.new(true, [4, 3])
-      bqueen.find_possible_moves(board.positions)
-      expect(bqueen.possible_moves.sort).to eq([[0, 0], [0, 4], [1, 1], [1, 4],
-                                                [1, 7], [2, 2], [2, 4], [2, 6],
-                                                [3, 3], [3, 4], [3, 5], [4, 1],
-                                                [4, 3], [4, 5], [4, 6], [4, 7],
-                                                [5, 3], [5, 4], [5, 5], [6, 2],
-                                                [6, 4], [6, 6], [7, 1], [7, 4],
-                                                [7, 7]])
+      nboard.positions[2][4] = Pawn.new(true, [2, 4])
+      nboard.positions[4][3] = Pawn.new(true, [4, 3])
+      bqueen.find_possible_moves(nboard.positions)
+      expect(bqueen.possible_moves.sort).to eq([[0, 0], [1, 1], [1, 7], [2, 2],
+                                                [2, 4], [2, 6], [3, 3], [3, 4],
+                                                [3, 5], [4, 3], [4, 5], [4, 6],
+                                                [4, 7], [5, 3], [5, 4], [5, 5],
+                                                [6, 2], [6, 4], [6, 6], [7, 1],
+                                                [7, 4], [7, 7]])
     end
   end
 end
-
